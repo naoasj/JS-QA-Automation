3127d40373b5749aa65a9df99468475cacc75739
Iryna Romanenko <iyrna.romaneko@dormakaba-external.com>
2026-07-23 12:54:47 +0200

    Homework Lesson 4

---
+++ Lesson4/jsBasics/task1.js
@@ -0,0 +1,14 @@
+/*Завдання 1: Оголошення змінних для примітивних типів
+
+Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: 
+рядок, число, буль, null та undefined. 
+Присвойте їм значення та виведіть кожну змінну на консоль*/
+
+let testString = "Рядок";
+let testNumber = 67;
+let testBoolian = true;
+let testNull = null;
+let testDefine = undefined;
+
+console.log(testString, testNumber, testBoolian, testNull, testDefine)
+
